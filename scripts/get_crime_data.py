56f65dd8d10eaf091c442ebce7d5683bd59ec990
MichaelShoemaker <mikeshoemaker2000@yahoo.com>
2022-09-01 11:12:41 -0500

    Added select for certain columns

--- scripts/get_crime_data.py
+++ scripts/get_crime_data.py
@@ -1,12 +1,32 @@
 import pandas as pd
 from sodapy import Socrata
 
+token = open('../secrets','r').readlines()[0].replace('\n','')
+
+
 client = Socrata("data.cityofchicago.org", token)
 
-query = """SELECT * WHERE community_id='14' and Date>='2021-01-01' \
-limit 20000000"""
+query = """SELECT date \
+    ,primary_type \
+    ,description \
+    ,location_description \
+    ,arrest \
+    ,domestic \
+    ,x_coordinate \
+    ,y_coordinate \
+    ,latitude \
+    ,longitude \
+    WHERE community_area='14' and Date>='2021-01-01' \
+    limit 20000000"""
 
-results = client.get("ijzp-q8t2")
+where = "community_id='14' and Date>='2021-01-01' limit 20000000"
+
+select = "'date ,primary_type, description,location_description,arrest,domestic ,x_coordinate,y_coordinate,latitude,longitude'"
+
+
+results = client.get("ijzp-q8t2", query=query)
 
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
+
+print(results_df.columns)

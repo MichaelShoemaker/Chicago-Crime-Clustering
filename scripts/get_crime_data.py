3cb860b99f13392eec07ac074e6e0f98c1672826
MichaelShoemaker <mikeshoemaker2000@yahoo.com>
2022-09-01 11:29:37 -0500

    Started Dockerfiles

--- scripts/get_crime_data.py
+++ scripts/get_crime_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sodapy import Socrata
 
-token = open('../secrets','r').readlines()[0].replace('\n','')
+token = open('secrets','r').readlines()[0].replace('\n','')
 
 
 client = Socrata("data.cityofchicago.org", token)
@@ -18,11 +18,6 @@ query = """SELECT date \
     ,longitude \
     WHERE community_area='14' and Date>='2021-01-01' \
     limit 20000000"""
-
-where = "community_id='14' and Date>='2021-01-01' limit 20000000"
-
-select = "'date ,primary_type, description,location_description,arrest,domestic ,x_coordinate,y_coordinate,latitude,longitude'"
-
 
 results = client.get("ijzp-q8t2", query=query)
 
